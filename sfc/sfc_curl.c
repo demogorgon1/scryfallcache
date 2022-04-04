6808220b5ada1797379fbb48330e2aa5c693c730
demogorgon1 <neckelmann@gmail.com>
2022-04-04 09:33:21 +0200

    - Removed some commented out code.

--- sfc/sfc_curl.c
+++ sfc/sfc_curl.c
@@ -24,15 +24,6 @@ typedef struct _sfc_curl_request
 	sfc_result					result;
 } sfc_curl_request;
 
-/*typedef struct _sfc_curl_write_context
-{
-	struct _sfc_app*	app;
-	char*				data;
-	size_t				size;
-	size_t				allocated;
-} sfc_curl_write_context;
-*/
-
 static size_t
 sfc_curl_write_function(
 	char*				ptr,
@@ -43,22 +34,6 @@ sfc_curl_write_function(
 	sfc_curl_request* req = (sfc_curl_request*)user_data;
 
 	sfc_buffer_append(&req->buffer, ptr, size * nmemb);
-
-	/*sfc_curl_write_context* write_context = (sfc_curl_write_context*)user_data;
-	size_t space_needed = size * nmemb;
-	size_t space_left = write_context->allocated - write_context->size;
-	
-	if(space_left < space_needed)
-	{
-		write_context->allocated += space_needed * 2;
-
-		write_context->data = SFC_ALLOC(write_context->app->alloc, write_context->app->user_data, write_context->data, write_context->allocated);
-		assert(write_context->data != NULL);
-	}
-
-	memcpy(write_context->data + write_context->size, ptr, space_needed);
-
-	write_context->size += space_needed;*/
 
 	return nmemb;
 }
@@ -144,14 +119,6 @@ sfc_curl_create(
 
 	curl_multi_setopt((CURLM*)context->curl_multi, CURLMOPT_MAX_TOTAL_CONNECTIONS, 1);
 
-	/*context->curl = (void*)curl_easy_init();
-	if (context->curl == NULL)
-	{	
-		sfc_debug(app, "curl_easy_init() failed");
-
-		return NULL;
-	}*/
-
 	return context;
 }
 
@@ -166,8 +133,6 @@ sfc_curl_destroy(
 	assert(context->requests.tail == NULL);
 
 	curl_multi_cleanup((CURLM*)context->curl_multi);
-
-	/*curl_easy_cleanup((CURL*)context->curl);*/
 
 	context->app->free(context->app->user_data, context);
 }
@@ -307,33 +272,6 @@ sfc_curl_get(
 	}
 
 	return req;
-
-	/*sfc_curl_write_context write_context;
-	memset(&write_context, 0, sizeof(sfc_curl_write_context));
-	write_context.app = context->app;
-
-	{
-		CURLcode res = curl_easy_perform(curl);
-		if (res != CURLE_OK)
-		{
-			sfc_debug(context->app, "curl_easy_perform() failed: %s (%s)", curl_easy_strerror(res), url);
-
-			result = SFC_RESULT_HTTP_GET_FAILED;
-		}
-	}
-
-	if(result == SFC_RESULT_OK)
-	{
-		*out_data = write_context.data;
-		*out_data_size = write_context.size;
-	}
-	else
-	{
-		if(write_context.data != NULL)
-			context->app->free(context->app->user_data, write_context.data);
-	}*/
-
-	/*return result;*/
 }
 
 sfc_bool
